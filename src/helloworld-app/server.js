20bf28965ff25f72142e45cecac7c13b99e931e9
Titiwut M <public@public.com>
2021-03-22 10:47:08 +0700

    add helloworld-app

---
+++ src/helloworld-app/server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT
+const host = process.env.HOST
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/healthz', (req, res) => {
+    res.send('online')
+  })
+
+app.listen(port, host, () => {
+  console.log(`Example app listening at ${host}:${port}`)
+})
